8351f96148f807439197318e948d94d9f4568561
Mark Greenwood <fatgerman@gmail.com>
2018-08-02 15:07:30 +0100

    How did that work?

--- includes/functions.php
+++ includes/functions.php
@@ -1125,16 +1125,14 @@ function check_reverse_slave_actions($cmds) {
     //
     // Re-check all add and playlistadd commands if we're using a Mopidy File Backend Slave
     //
-    if ($prefs['mopidy_slave']) {
-        foreach ($cmds as $key => $cmd) {
-            // add "local:track:
-            // playlistadd "local:track:
-            if (substr($cmd, 0, 12) == 'add "file://' ||
-                substr($cmd, 0,20) == 'playlistadd "file://') {
-                $cmds[$key] = swap_file_for_local($cmd);
-            }
-
+    foreach ($cmds as $key => $cmd) {
+        // add "local:track:
+        // playlistadd "local:track:
+        if (substr($cmd, 0, 12) == 'add "file://' ||
+            substr($cmd, 0,20) == 'playlistadd "file://') {
+            $cmds[$key] = swap_file_for_local($cmd);
         }
+
     }
     return $cmds;
 }
