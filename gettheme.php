68302a6ad9e9ec502620d70f2537e319464fa901
Mark Greenwood <fatgerman@gmail.com>
2019-07-13 23:40:24 +0100

    pish

--- gettheme.php
+++ gettheme.php
@@ -6,9 +6,10 @@ logger::log('THEME','Font Size   :',$_REQUEST['fontsize']);
 logger::log('THEME','Cover Size  :',$_REQUEST['coversize']);
 logger::log('THEME','Icons       :',$_REQUEST['icontheme']);
 header('Content-Type: text/css');
+// Font MUST be first, otherwise the @import on the Google fonts doesn't work
+readfile('fonts/'.$_REQUEST['fontfamily']);
 readfile('themes/'.$_REQUEST['theme']);
 readfile('sizes/'.$_REQUEST['fontsize']);
-readfile('fonts/'.$_REQUEST['fontfamily']);
 readfile('coversizes/'.$_REQUEST['coversize']);
 readfile('iconsets/'.$_REQUEST['icontheme'].'/theme.css');
 readfile('iconsets/'.$_REQUEST['icontheme'].'/adjustments.css');
