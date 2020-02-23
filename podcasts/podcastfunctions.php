b7c5d53a159fe4e83e6b65c8b9b55a4c3551bf59
Mark Greenwood <fatgerman@gmail.com>
2020-02-23 11:17:13 +0000

    Wierd behaviour with podcast updates on MySQL

--- podcasts/podcastfunctions.php
+++ podcasts/podcastfunctions.php
@@ -508,7 +508,7 @@ function refreshPodcast($podid) {
 			sql_prepare_query(true, null, null, null, "UPDATE PodcastTracktable SET JustUpdated=?, Duration=?, Link=? WHERE PODTrackindex=?",1,$track['Duration'], $track['Link'], $trackid);
 		} else {
 			if (sql_prepare_query(true, null, null, null,
-				mb4_bodge()."INSERT INTO PodcastTracktable
+				"INSERT INTO PodcastTracktable
 				(JustUpdated, PODindex, Title, Artist, Duration, PubDate, FileSize, Description, Link, Guid, New)
 				VALUES
 				(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
