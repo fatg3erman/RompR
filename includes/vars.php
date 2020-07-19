19728b96ddd3e2d7f6fda6483d2aa425f1a1560d
Mark Greenwood <fatgerman@gmail.com>
2020-07-19 14:39:06 +0100

    itier

--- includes/vars.php
+++ includes/vars.php
@@ -9,7 +9,7 @@ define('ROMPR_MAX_TRACKS_PER_TRANSACTION', 500);
 define('ROMPR_COLLECTION_VERSION', 6);
 define('ROMPR_IMAGE_VERSION', 4);
 define('ROMPR_SCHEMA_VERSION', 67);
-define('ROMPR_VERSION', '1.44');
+define('ROMPR_VERSION', '1.45');
 define('ROMPR_IDSTRING', 'RompR Music Player '.ROMPR_VERSION);
 define('ROMPR_MOPIDY_MIN_VERSION', 1.1);
 define('ROMPR_UNKNOWN_STREAM', "Unknown Internet Stream");
