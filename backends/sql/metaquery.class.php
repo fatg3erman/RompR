63971dc53d408d87503082f499fdc24254caa09c
Mark Greenwood <fatgerman@gmail.com>
2021-04-25 21:08:17 +0100

    paff

--- backends/sql/metaquery.class.php
+++ backends/sql/metaquery.class.php
@@ -266,7 +266,7 @@ class metaquery extends collection_base {
 				break;
 
 			case CHARTS_AUDIOBOOKS_ONLY:
-				return ' WHERE isAudiobook = 1 AND Hidden = 0 ';
+				return ' WHERE isAudiobook > 0 AND Hidden = 0 ';
 				break;
 
 			case CHARTS_INTERNET_ONLY:
