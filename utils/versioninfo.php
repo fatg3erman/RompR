39d53d5bf88f8fa5b8e400d4f2f8b0ecc9a648c8
Mark Greenwood <fatgerman@gmail.com>
2022-01-08 21:23:32 +0000

    ,hj

--- utils/versioninfo.php
+++ utils/versioninfo.php
@@ -1,9 +1,11 @@
 <?php
 chdir('..');
 include ('includes/vars.php');
-if (file_exists('updateinfo/'.ROMPR_VERSION.'/info.html')) {
-	readfile('updateinfo/'.ROMPR_VERSION.'/info.html');
-} else {
-	header('HTTP/1.1 204 No Content');
+if (preg_match('/^(\d+\.\d+)/', ROMPR_VERSION, $matches)) {
+	if (file_exists('updateinfo/'.$matches[1].'/info.html')) {
+		readfile('updateinfo/'.$matches[1].'/info.html');
+		exit(0);
+	}
 }
+header('HTTP/1.1 204 No Content');
 ?>
