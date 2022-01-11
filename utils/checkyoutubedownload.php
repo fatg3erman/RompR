7763f9c946c8d4622f691652f1d4b7cfb69423f4
Mark Greenwood <fatgerman@gmail.com>
2022-01-11 16:52:25 +0000

    hd

--- utils/checkyoutubedownload.php
+++ utils/checkyoutubedownload.php
@@ -7,7 +7,7 @@ if (file_exists('prefs/youtubedl/dlprogress_'.$_REQUEST['key'])) {
 	$reader = new minotaur('prefs/youtubedl/dlprogress_'.$_REQUEST['key']);
 	$r = $reader->get_last_line_of_file();
 	if ($r !== false) {
-		if (preg_match('/\[ffmpeg\]\s*Destination:\s*(.+)$/', $r, $matches)) {
+		if (preg_match('/\[ffmpeg\]\s*Destination:\s*(.+)$/', $r, $matches) && file_exists($matches[1])) {
 			$retval = 'Writing Audio : Written '.format_bytes(filesize(trim($matches[1]))).'bytes';
 		} else {
 			$retval = $r;
