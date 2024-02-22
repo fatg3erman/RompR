6a75165b586d70b3ef572db58d0332b3ff02e828
Mark Greenwood <fatgerman@gmail.com>
2024-02-22 14:06:30 +0000

    Tidy formatting

--- player/utils/outputs.php
+++ player/utils/outputs.php
@@ -28,8 +28,11 @@ function printOutputCheckboxes() {
 			print ' checked';
 		}
 		print "><label for=\"outputbutton_${i}\" onclick=\"player.controller.doOutput(${i})\">";
-		if ( ! preg_match('/^http[s]*:\/\/.+$/', $outputdata[$i]['outputname'], $matches) ) { print $outputdata[$i]['outputname']; }
-		else { print $outputdata[$i]['outputname'] . "	<a href=\"". $outputdata[$i]['outputname'] . "\" target=\"_blank\"> &#128279;</a></label>"; }
+		if (!preg_match('/^http[s]*:\/\/.+$/', $outputdata[$i]['outputname'], $matches)) {
+			print $outputdata[$i]['outputname'];
+		} else {
+			print $outputdata[$i]['outputname'] . "	<a href=\"". $outputdata[$i]['outputname'] . "\" target=\"_blank\"> &#128279;</a></label>";
+		}
 		print '</div>';
 	}
 }
