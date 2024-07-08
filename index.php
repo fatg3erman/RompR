1648650f030f8f2b6c5d58b15821626980c467bf
Mark Greenwood <fatgerman@gmail.com>
2024-07-08 20:06:42 +0100

    dgd

--- index.php
+++ index.php
@@ -212,7 +212,7 @@ logger::mark("CREATING PAGE", "******++++++======------******------======++++++*
 <link rel="shortcut icon" sizes="16x16" href="newimages/favicon-16.png" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
 <meta name="apple-mobile-web-app-capable" content="yes" />
-<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+<meta name="apple-mobile-web-app-status-bar-style" content="default" />
 <meta name="mobile-web-app-capable" content="yes" />
 <?php
 print '<script type="application/json" name="custom_radio_items">'."\n".json_encode(CUSTOM_RADIO_ITEMS)."\n</script>\n";
