448a0a5c81699d8753d261589178bddbaa8d1fa9
Mark Greenwood <fatgerman@gmail.com>
2026-06-19 21:13:45 +0100

    hdh

--- skins/prefspanel.class.php
+++ skins/prefspanel.class.php
@@ -391,9 +391,9 @@ class prefspanel extends uibits {
 				'name' => 'dinkeyking'
 			];
 		}
-		if (!prefs::get_player_param('remote')) {
+		// if (!prefs::get_player_param('remote')) {
 			self::ui_config_button($update_buttons);
-		}
+		// }
 
 		//
 		// Album Sorting
